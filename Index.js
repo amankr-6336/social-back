706288c99cdb755a6540c45065ef694787aba166
amankr.6336 <amankr.17709418@gmail.com>
2023-04-24 15:06:59 +0530

    what

--- Index.js
+++ Index.js
@@ -31,7 +31,7 @@ app.use(express.json({limit:'10mb'}));
 app.use(cookieParser());
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:3000'
+    origin:process.env.CORS_ORIGIN
 }))
 
 
